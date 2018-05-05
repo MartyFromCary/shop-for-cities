130d925b4d858ed2d649c52746278d4ab1dace84
Deb Bayo <30481103+DABAYO@users.noreply.github.com>
2018-05-05 15:36:46 -0400

    Updated Search.js
    added line 3  from Marty's 2nd Try.

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,5 +1,6 @@
   import React, { Component } from "react";
   import styled from "styled-components";
+  import { Link } from "react-router-dom";
 
   import Jumbotron from "../../components/Jumbotron";
   import { Col, Row, Container } from "../../components/Grid";
